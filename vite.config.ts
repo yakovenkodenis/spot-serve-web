e52880ef60086b6e418ac02dfa66a154783677b5
Bohdan Andrianov <bogd.an@proverstka.com>
2025-01-22 19:45:41 +0800

    Constant baseUrl for dev and prod modes

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : '/spot-serve-web',
+export default defineConfig(() => ({
+  base: '/spot-serve-web',
 
   resolve: {
     alias: {
